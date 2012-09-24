08a7b2236196fe558c078bee5ca519c9412473c6
Jakob Færch <jrf@trifork.com>
2012-09-24 15:39:58 +0200

    Deploy som modul i jboss inkl. simpel int.test

---
+++ src/main/java/dk/nsi/sdm4/ydelse/config/YdelseimporterInfrastructureConfig.java
@@ -0,0 +1,50 @@
+/**
+ * The MIT License
+ *
+ * Original work sponsored and donated by National Board of e-Health (NSI), Denmark
+ * (http://www.nsi.dk)
+ *
+ * Copyright (C) 2011 National Board of e-Health (NSI), Denmark (http://www.nsi.dk)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package dk.nsi.sdm4.ydelse.config;
+
+import dk.nsi.sdm4.core.config.StamdataConfiguration;
+import dk.sdsd.nsp.slalog.api.SLALogConfig;
+import dk.sdsd.nsp.slalog.api.SLALogger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Spring-konfiguration der ikke forventes at ændre sig mellem miljøer (fx test/produktion)
+ */
+@Configuration
+@EnableScheduling
+@EnableTransactionManagement
+//The Spring Java Configuration annotations above needs to be on this class, not on the abstract superclass to
+// make Spring stop complaining about weird things
+public class YdelseimporterInfrastructureConfig extends StamdataConfiguration {
+	@Bean
+	public SLALogger slaLogger() {
+		return new SLALogConfig("Stamdata SKS-importer", "sksimporter").getSLALogger();
+	}
+}
